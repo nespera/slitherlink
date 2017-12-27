dc4de4bd2b00287df068ac8421d33f03ac4a4472
chrisr <c@24.org.uk>
2017-12-27 16:26:43 +0000

    Add segment state to board state string representation

--- src/main/scala/uk/me/chrs/slitherlink/BoardState.scala
+++ src/main/scala/uk/me/chrs/slitherlink/BoardState.scala
@@ -1,14 +1,17 @@
 package uk.me.chrs.slitherlink
 
-class BoardState(val board: Board, val state: Map[Segment, Option[Boolean]]) {
-
+class BoardState(val board: Board, segmentStates: Map[Segment, Option[Boolean]]) {
 
   override def toString: String = {
-    def points(r: Int): String = Seq.fill(board.width + 1)(".").mkString(" ") + "\n"
+    def points(row: Int): String = {
+      horizontals(row).map(makeSegmentString("_", _))
+        .mkString(".", ".", ".\n")
+    }
 
-    def squares(r: Int): String = {
-      val row = board.getRow(r)
-      " " + row.map(s => s.target.map(_.toString).getOrElse(" ")).mkString(" ") + " " + "\n"
+    def squares(row: Int): String = {
+      val values = board.getRow(row).map(s => s.target.map(_.toString).getOrElse(" ")) :+ "\n"
+      val verts = verticals(row).map(makeSegmentString("|", _))
+      intersperse(verts, values).mkString
     }
 
     val s = new StringBuilder
@@ -16,9 +19,27 @@ class BoardState(val board: Board, val state: Map[Segment, Option[Boolean]]) {
       s.append(points(r))
       s.append(squares(r))
     }
-    s.append(points(board.height + 1))
+    s.append(points(board.height))
     s.toString()
   }
+
+  private def makeSegmentString(str: String, s: Segment) = if (segmentStates(s).contains(true)) str else " "
+
+  private def verticals(row: Int) = {
+    board.segments.filter(s => s.points.exists(_.x == row) && s.points.exists(_.x == row + 1)).toSeq
+      .sortBy(_.points.head.y)
+  }
+
+  private def horizontals(row: Int) = {
+    board.segments.filter(s => s.points.forall(_.x == row)).toSeq
+      .sortBy(_.points.head.y)
+  }
+
+  def updated(a: Point, b: Point, state: Option[Boolean]): BoardState = {
+    new BoardState(board, segmentStates.updated(Segment(a, b), state))
+  }
+
+  private def intersperse(l1: Seq[String], l2: Seq[String]) = l1.zip(l2) flatMap { case (a, b) => Seq(a, b) }
 }
 
 object BoardState {
--- src/test/scala/uk/me/chrs/slitherlink/BoardStateSpec.scala
+++ src/test/scala/uk/me/chrs/slitherlink/BoardStateSpec.scala
@@ -5,8 +5,15 @@ import org.specs2.mutable.Specification
 class BoardStateSpec extends Specification {
 
   "A board state" should {
+    val state = BoardState.empty(2, 3, ".12..3")
     "be convertible to string" in {
-      BoardState.empty(2, 3, ".12..3").toString mustEqual ". . . .\n   1 2 \n. . . .\n     3 \n. . . .\n"
+      state.toString mustEqual ". . . .\n   1 2 \n. . . .\n     3 \n. . . .\n"
+
+      val state2 = state.updated(Point(0, 0), Point(0, 1), Some(true))
+        .updated(Point(0, 0), Point(1, 0), Some(true))
+        .updated(Point(1,1), Point(1, 2), Some(false))
+
+      state2.toString mustEqual "._. . .\n|  1 2 \n. . . .\n     3 \n. . . .\n"
     }
   }
 }
