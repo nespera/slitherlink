54e50533b38d3b60a52b435b1ea485895ae815f3
chrisr <c@24.org.uk>
2017-12-27 14:26:00 +0000

    Add target values read from a string

--- src/main/scala/uk/me/chrs/slitherlink/Board.scala
+++ src/main/scala/uk/me/chrs/slitherlink/Board.scala
@@ -1,12 +1,34 @@
 package uk.me.chrs.slitherlink
 
-class Board(width: Int, height: Int) {
-  val squares: Set[Square] = (for { x <- 0 until width; y <- 0 until height } yield Square(x, y)).toSet
-  val points: Set[Point] = (for { x <- 0 to width; y <- 0 to height } yield Point(x, y)).toSet
+/*
+ Coordinates on the board run as follows:
+
+ (0,0) ..... (0,n)
+  .           .
+  .           .
+ (n,0) ..... (n,n)
+
+ */
+
+class Board(height: Int, width: Int, targets: String) {
+  if(targets.length != width * height) throw new IllegalArgumentException(s"Targets is of length ${targets.length}, expected ${width * height}")
+  val squares: Set[Square] = (for { x <- 0 until height; y <- 0 until width } yield Square(x, y, targetAt(x,y))).toSet
+  val points: Set[Point] = (for { x <- 0 to height; y <- 0 to width } yield Point(x, y)).toSet
   val segments: Set[Segment] = points.flatMap(p => points.filter(p.adjacent).map(Segment(p, _)))
+  private def targetAt(x: Int, y: Int): Option[Int] = {
+    targets(x*width + y) match {
+      case '0' => Some(0)
+      case '1' => Some(1)
+      case '2' => Some(2)
+      case '3' => Some(3)
+      case '.' => None
+      case c => throw  new IllegalArgumentException(s"Bad target character: $c")
+    }
+
+  }
 }
 
-case class Square(x: Int, y: Int)
+case class Square(x: Int, y: Int, target: Option[Int])
 case class Point(x: Int, y: Int){
   def adjacent(other: Point) : Boolean = {
     (x == other.x && Math.abs(y-other.y) == 1) || (y == other.y && Math.abs(x-other.x) == 1)
--- src/test/scala/uk/me/chrs/slitherlink/BoardSpec.scala
+++ src/test/scala/uk/me/chrs/slitherlink/BoardSpec.scala
@@ -6,19 +6,33 @@ class BoardSpec extends Specification {
 
   "A board" should {
 
+    "have targets for all its squares" in {
+      new Board(1, 1, "") must throwAn[IllegalArgumentException]
+    }
+
+    "have targets 0,1,2,3 or a dot for no target" in {
+      new Board(1, 1, "4") must throwAn[IllegalArgumentException]
+    }
+
     "Have the expected squares" in {
-      new Board(0, 0).squares mustEqual Set()
-      new Board(2, 3).squares mustEqual Set(Square(0,0), Square(1,0), Square(0,1), Square(1,1), Square(0,2), Square(1,2))
+      new Board(0, 0, "").squares mustEqual Set()
+      new Board(2, 3, ".0123.").squares mustEqual Set(
+        Square(0,0, None),
+        Square(0,1, Some(0)),
+        Square(0,2, Some(1)),
+        Square(1,0, Some(2)),
+        Square(1,1, Some(3)),
+        Square(1,2, None))
     }
 
     "Have the expected points" in {
-      new Board(0, 0).points mustEqual Set(Point(0,0))
-      new Board(1, 2).points mustEqual Set(Point(0,0), Point(1,0), Point(0,1), Point(1,1), Point(0,2), Point(1,2))
+      new Board(0, 0, "").points mustEqual Set(Point(0,0))
+      new Board(1, 2, "12").points mustEqual Set(Point(0,0), Point(0,1), Point(0,2), Point(1,0), Point(1,1), Point(1,2))
     }
 
     "Have the expected segments" in {
-      new Board(0, 0).segments mustEqual Set()
-      new Board(1, 1).segments mustEqual Set(
+      new Board(0, 0, "").segments mustEqual Set()
+      new Board(1, 1, "0").segments mustEqual Set(
         Segment(Point(0,0), Point(0,1)),
         Segment(Point(0,0), Point(1,0)),
         Segment(Point(0,1), Point(1,1)),
