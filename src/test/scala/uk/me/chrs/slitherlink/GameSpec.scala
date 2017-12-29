a08154cc9842fde2c1cfc1f20cc1af574302630a
chrisr <c@24.org.uk>
2017-12-29 17:23:57 +0000

    Fix tests to match new output

--- src/test/scala/uk/me/chrs/slitherlink/GameSpec.scala
+++ src/test/scala/uk/me/chrs/slitherlink/GameSpec.scala
@@ -16,35 +16,33 @@ class GameSpec extends Specification {
       val solved = Game.solve(state)
       solved must beSome
       solved.get.toString.trim mustEqual
-        """>+-+-+
+        """>•‒•‒•
            >|3  |
-           >+-+-+""".stripMargin('>')
+           >•‒•‒•""".stripMargin('>')
     }
 
     "find a solution for a small problem" in {
       val state = BoardState.empty(1, 3, ".33")
-      println(state)
       val solved = Game.solve(state)
       solved must beSome
       solved.get.toString.trim mustEqual
-        """>+ +-+-+
+        """>• •‒•‒•
            >  |3 3|
-           >+ +-+-+""".stripMargin('>')
+           >• •‒•‒•""".stripMargin('>')
     }
 
     "find a solution for a smallish problem" in {
       val state = BoardState.empty(3, 3, "...303...")
-      println(state)
       val solved = Game.solve(state)
       solved must beSome
       solved.get.toString.trim mustEqual
-        "+ +-+ +\n"+
+        "• •‒• •\n"+
         "  | |  \n"+
-        "+-+ +-+\n"+
+        "•‒• •‒•\n"+
         "|3 0 3|\n"+
-        "+-+ +-+\n"+
+        "•‒• •‒•\n"+
         "  | |  \n"+
-        "+ +-+ +"
+        "• •‒• •"
     }
   }
 
